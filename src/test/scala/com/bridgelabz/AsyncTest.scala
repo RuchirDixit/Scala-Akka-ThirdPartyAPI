02bf0dfe35e423a377a4a9b5981bfee2ecbc5ff8
RuchirDixit <ruchirtd96@gmail.com>
2020-12-17 20:53:36 +0530

    [Ruchir]Added: Test cases to check valid name

--- src/test/scala/com/bridgelabz/AsyncTest.scala
+++ src/test/scala/com/bridgelabz/AsyncTest.scala
@@ -7,11 +7,11 @@ import scala.concurrent.Future
 class AsyncTest extends AsyncFlatSpec {
   behavior of "sayHelloTo"
   it should "return a valid message on a valid name" in {
-    val futureHarry : Future[Option[String]] = AsynchMain.sayHelloTo("Harry")
+    val futureHarry : Future[Option[String]] = AsyncMain.sayHelloTo("Harry")
     futureHarry map { res => assert(res == Some("Hello Harry, welcome to the future world!")) }
   }
   it should "return a valid message on an invalid name" in {
-    val futureVoldemort : Future[Option[String]] = AsynchMain.sayHelloTo("Voldemort")
+    val futureVoldemort : Future[Option[String]] = AsyncMain.sayHelloTo("Voldemort")
     futureVoldemort map { res => assert(res == None) }
   }
 }
