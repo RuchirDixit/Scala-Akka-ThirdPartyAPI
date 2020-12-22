7552131e6fc1e494b7f533af6ae05615aaa41e04
RuchirDixit <ruchirtd96@gmail.com>
2020-12-23 00:46:19 +0530

    [Ruchir]Added: Test case for actors

--- src/test/scala/com/bridgelabz/AsyncTest.scala
+++ src/test/scala/com/bridgelabz/AsyncTest.scala
@@ -1,10 +1,14 @@
 package com.bridgelabz
 
-import org.scalatest.flatspec.AsyncFlatSpec
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{TestActorRef, TestProbe}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.flatspec.{AnyFlatSpec, AsyncFlatSpec}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class AsyncTest extends AsyncFlatSpec {
+class AsyncTest extends AnyFlatSpec {
   behavior of "sayHelloTo"
   it should "return a valid message on a valid name" in {
     val futureHarry : Future[Option[String]] = AsyncMain.sayHelloTo("Harry")
@@ -14,4 +18,13 @@ class AsyncTest extends AsyncFlatSpec {
     val futureVoldemort : Future[Option[String]] = AsyncMain.sayHelloTo("Voldemort")
     futureVoldemort map { res => assert(res == None) }
   }
+
+  it should "return true if string is passed" in {
+    implicit val system = ActorSystem("TestActor")
+    val probe = TestProbe()
+    val testActor = TestActorRef[DownloadActor]
+    probe.send(testActor,"DownloadData")
+    probe.expectMsg(true)
+  }
+
 }
