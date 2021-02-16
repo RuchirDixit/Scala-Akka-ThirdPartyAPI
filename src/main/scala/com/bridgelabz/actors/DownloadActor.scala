d1f76083b8c6458f678fc77275fc61626572067f
RuchirDixit <ruchirtd96@gmail.com>
2021-02-16 20:02:04 +0530

    [Ruchir]Refactored: Removed hardcoded values for url

--- src/main/scala/com/bridgelabz/actors/DownloadActor.scala
+++ src/main/scala/com/bridgelabz/actors/DownloadActor.scala
@@ -17,10 +17,11 @@ package com.bridgelabz.actors
 
 import akka.actor.Actor
 import akka.event.Logging
+import com.bridgelabz.main.HttpClientSingleRequest
 // Actors to receive string message and print result
 class DownloadActor extends Actor{
   val log = Logging(context.system, this)
-  val url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=" + sys.env("key")
+  val url = HttpClientSingleRequest.url
   override def receive: Receive = {
     case _:String => log.info("Downloading data from " + url)
                     sender() ! true
